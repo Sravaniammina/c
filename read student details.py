f8de25e7bda5cf8423c934da71fc4586b7ecf79b
Sravaniammina <126048393+Sravaniammina@users.noreply.github.com>
2023-10-10 19:55:07 +0530

    Create read student details.py

---
+++ read student details.py
@@ -0,0 +1,15 @@
+#Assognment-3
+#Write a program to read sno,sname,group,3subjects and print total and avg
+int(input("s1:"))
+str(input("sname:"))
+str(input("group:"))
+str(input("subject_1:"))
+str(input("subject_2:"))
+str(input("subject_3:"))
+int(input("s1_marks:"))
+int(input("s2_marks:"))
+int(input("s3_marks:"))
+total=s1_marks+s2_marks+s3_marks
+print(total)
+avg=total/3
+print(avg)
